6c3a06318d18165b17bf443dbeda232e1d78f378
Si Yujun <siuj0810@gmail.com>
2024-06-21 00:36:36 +0900

    google login

---
+++ lib/src/app.dart
@@ -0,0 +1,21 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flufire_sample_001/src/pages/home.dart';
+import 'package:flufire_sample_001/src/pages/login.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (_, AsyncSnapshot<User?> user) {
+          if (user.hasData) {
+            return const Home();
+          } else {
+            return const Login();
+          }
+        });
+  }
+}
